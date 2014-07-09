8edaa311a2fde3347ac94d09856e0a26b1085cae
ralphdoncaster <ralphdoncaster@3b18934d-bc59-68fa-a9ef-1802fb6f2968>
2014-07-09 15:35:39 +0000

    tested version

--- trunk/avr/timer-asm.c
+++ trunk/avr/timer-asm.c
@@ -5,26 +5,33 @@
 #define byte unsigned char
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 // seconds defined in assembler code
 extern unsigned long __system_time;
+extern byte ovfl_count;
 
 // to-do: map prescaler /32 thru /256 to 2^CSn
-#define CSDIV256 (1<<CS22) | (1<<CS21)
+#define CSDIV256 ((1<<CS22) | (1<<CS21))
 
 void main()
 {
+    __system_time = 0;
 
     // normal mode, clear counter when count reached 
-
     TCCR2A = (1<<WGM21);
     TCCR2B = CSDIV256;
 
     OCR2A = 250;                // reset timer when count reached
     TIMSK2 = (1<<OCIE2A);       // enable intterupt
+    sei();
 
     DDRB = 0xff;                // output mode
     while (1) {
         PORTB = __system_time & 0xff;
+#ifdef DEBUG
+        PORTB = ovfl_count & 0xff;
+        PINB = 0x01;            // toggle pin for debug
+#endif
     }
 }
