3f9d212086da10f1511a3ae8ab9a1bea23c15a56
ralphdoncaster <ralphdoncaster@3b18934d-bc59-68fa-a9ef-1802fb6f2968>
2014-08-10 00:15:18 +0000

    add comments

--- trunk/avr/uart_echo.c
+++ trunk/avr/uart_echo.c
@@ -1,13 +1,18 @@
-#define BAUD_RATE 38400
+/* BBUart example program
+ * compile with -DBAUD_RATE=57600 to use 57.6kbps
+ * see BBUart.h for default when BAUD_RATE is undefined
+ */ 
+
 #include "BBUart.h"
 
 const unsigned char EOF = 0x04;
 
 void main(void)
 {
-    unsigned char val = 42;
-    while ( (val = RxByte()) != EOF ){
-        TxByte(val);
+    unsigned char c = ':';
+    TxByte(c);
+    while ( (c = RxByte()) != EOF ){
+        TxByte(c);
     }
 }
 
