9fa72d83468967181ccff16c39714177796540b6
ralphdoncaster <ralphdoncaster@3b18934d-bc59-68fa-a9ef-1802fb6f2968>
2014-08-22 19:05:24 +0000

    comment out default settings

--- trunk/avr/rfout.c
+++ trunk/avr/rfout.c
@@ -4,20 +4,22 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main()
+__attribute__ ((OS_main)) int main()
 {
   // OSCCAL -= 6; // gives ~8Mhz @ 5V
   OSCCAL += 23; // gives 9.86Mhz
   PLLCSR = (1<<PCKE) | (1<<PLLE); // turn on PLL
   // setup timer1
-//  OCR1A = 0; // match count 
-  OCR1C = 0; // count to
+//  OCR1A = 0; // match count - reset value = 0
+//  OCR1C = 16; // count to
+// OCR1C reset value = 255
   DDRB = (1<<PB1);  // set OC1A to output
   while(1){
+    // 150 + 130 us =~ 3.5kHz cycle tone generator
     TCCR1 = (1<<CTC1)|(1<<COM1A0)|(1<<CS10);
-    _delay_ms(30);
-    TCCR1 = 0;
-    PORTB = 0;
-    _delay_ms(50);
+    _delay_us(150);
+//    TCCR1 = 0;
+//    PORTB = 0;
+    _delay_us(130);
   }
 }
