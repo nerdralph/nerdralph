46b6b7b8df3d96cbb3131d80b72e92386a00e6a2
unknown <ralphdoncaster@gmail.com>
2015-04-12 23:55:09 -0300

    add -quiet to wget

--- avr/make-avr-gcc/getfiles.gcc.sh
+++ avr/make-avr-gcc/getfiles.gcc.sh
@@ -24,57 +24,57 @@ source ${base}/package-versions
 #
 # Get GDB
 #
-#wget -c  ftp://sourceware.org/pub/insight/releases/${insighttar}
-#wget -c ftp://ftp.sunet.se/pub/gnu/gdb/${gdbtar}
+#wget -cq  ftp://sourceware.org/pub/insight/releases/${insighttar}
+#wget -cq ftp://ftp.sunet.se/pub/gnu/gdb/${gdbtar}
 
 #
 # Get avarice
 #
-#wget -c  http://downloads.sourceforge.net/avarice/${avaricetar}
+#wget -cq  http://downloads.sourceforge.net/avarice/${avaricetar}
 
 #
 # Get avrdude
 #
-#wget -c  http://download.savannah.gnu.org/releases/avrdude/${avrdudetar}
-#wget -c  http://download.savannah.gnu.org/releases/avrdude/avrdude-doc-${avrdudever}.pdf
+#wget -cq  http://download.savannah.gnu.org/releases/avrdude/${avrdudetar}
+#wget -cq  http://download.savannah.gnu.org/releases/avrdude/avrdude-doc-${avrdudever}.pdf
 
 #
 # Get Binutils
 #
-#wget -c  ftp://ftp.dkuug.dk/pub/gnu/ftp/gnu/binutils/binutils-2.19.1.tar.bz2
-wget -c  http://ftp.gnu.org/gnu/binutils/${binutilstar}
+#wget -cq  ftp://ftp.dkuug.dk/pub/gnu/ftp/gnu/binutils/binutils-2.19.1.tar.bz2
+wget -cq  http://ftp.gnu.org/gnu/binutils/${binutilstar}
 
 #
 # Get avr-libc
 #
 #
-#wget -c  http://download.savannah.gnu.org/releases/avr-libc/avr-libc-user-manual-${avrlibcver}.pdf.bz2
-wget -c  http://download.savannah.gnu.org/releases/avr-libc/${avrlibctar}
+#wget -cq  http://download.savannah.gnu.org/releases/avr-libc/avr-libc-user-manual-${avrlibcver}.pdf.bz2
+wget -cq  http://download.savannah.gnu.org/releases/avr-libc/${avrlibctar}
 
 # GCC Pre-requisites - see https://gcc.gnu.org/install/prerequisites.html
 
 #
 # Get GMP
 #
-#wget -c http://ftp.sunet.se/pub/gnu/gmp/gmp-4.2.4.tar.bz2
-wget -c http://ftp.sunet.se/pub/gnu/gmp/${gmptar}
+#wget -cq http://ftp.sunet.se/pub/gnu/gmp/gmp-4.2.4.tar.bz2
+wget -cq http://ftp.sunet.se/pub/gnu/gmp/${gmptar}
 
 #
 # Get MPFR
 #
-#wget -c http://www.mpfr.org/mpfr-2.4.1/${mpfrtar}
-wget -c http://ftp.sunet.se/pub/gnu/mpfr/${mpfrtar}
+#wget -cq http://www.mpfr.org/mpfr-2.4.1/${mpfrtar}
+wget -cq http://ftp.sunet.se/pub/gnu/mpfr/${mpfrtar}
 
 #
 # Get MPC 
 #
-wget -c http://www.multiprecision.org/mpc/download/${mpctar}
+wget -cq http://www.multiprecision.org/mpc/download/${mpctar}
 
 #
 #Get GCC
 #
-#wget -c  ftp://ftp.dkuug.dk/pub/gnu/ftp/gnu/gcc/gcc-4.3.2/gcc-4.3.2.tar.bz2
-wget -c  http://ftp.gnu.org/gnu/gcc/${gcccore}/${gcccoretar}
+#wget -cq  ftp://ftp.dkuug.dk/pub/gnu/ftp/gnu/gcc/gcc-4.3.2/gcc-4.3.2.tar.bz2
+wget -cq  http://ftp.gnu.org/gnu/gcc/${gcccore}/${gcccoretar}
 
 #
 # Done
