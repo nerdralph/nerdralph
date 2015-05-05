4246ffeb4236cfccda9b0cf689d30e0a4f4602f2
unknown <ralphdoncaster@gmail.com>
2015-05-04 22:34:01 -0300

    add spi_in and spi_out

--- avr/halfduplexspi.h
+++ avr/halfduplexspi.h
@@ -7,6 +7,10 @@
  *  MOMI --+-------- MOSI
  *         +-\/\/\-- MISO
  *            330
+ *
+ * define TDDSPI before including this file to enable time-division
+ * duplexing.  Otherwise set SPI_MOMI to input or output mode before
+ * calling spi_byte.
  */
 
 
@@ -20,24 +24,65 @@
 #define SPI_DDR (*((&SPI_PORT) -1))
 #define SPI_PIN (*((&SPI_PORT) -2))
 
-uint8_t spi_byte(uint8_t byte){
+void spi_setup()
+{
+    sbi (SPI_DDR, SPI_SCK);             // output mode
+    //sbi (MCUCR, PUD);                   // disable pullup
+}
 
-    uint8_t i = 8;
-    sbi (SPI_DDR, SPI_SCK);        // output mode
+uint8_t spi_byte(uint8_t dataout){
+
+    uint8_t datain, bits = 8;
     
     do{
+        datain <<= 1;                     
+        if(SPI_PIN & (1<<SPI_MOMI)) datain++;
+#ifdef TDDSPI
         sbi (SPI_DDR, SPI_MOMI);        // output mode
-        cbi (SPI_PORT, SPI_MOMI);
-        if(byte & 0x80) sbi (SPI_PORT, SPI_MOMI);
-        sbi (SPI_PORT, SPI_SCK);
+#endif
+        //if ((dataout & 0x80) == 0) cbi (SPI_PORT, SPI_MOMI);
+        if (dataout & 0x80) sbi (SPI_PORT, SPI_MOMI);
+        SPI_PIN = (1<<SPI_SCK);
+#ifdef TDDSPI
         cbi (SPI_DDR, SPI_MOMI);        // input mode
-        byte <<= 1;                     
-        asm("nop");
-        if(SPI_PIN & (1<<SPI_MOMI)) byte++;
-        cbi(SPI_PORT, SPI_SCK);
+        //asm("nop");
+#endif
+        SPI_PIN = (1<<SPI_SCK);         // toggle SCK
+        cbi (SPI_PORT, SPI_MOMI);
+        dataout <<= 1;                     
     
-    }while(--i);
+    }while(--bits);
 
-    return byte;
+    return datain;
+}
+
+uint8_t spi_in(void){
+
+    uint8_t datain, bits = 8;
+    
+    do{
+        datain <<= 1;                     
+        SPI_PIN = (1<<SPI_SCK);
+        SPI_PIN = (1<<SPI_SCK);         // toggle SCK
+        if(SPI_PIN & (1<<SPI_MOMI)) datain++;
+    }while(--bits);
+
+    return datain;
+}
+
+void spi_out(uint8_t dataout){
+
+    sbi (SPI_DDR, SPI_MOMI);        // output mode
+    uint8_t bits = 8;
+    
+    do{
+        if (dataout & 0x80) SPI_PIN = (1<<SPI_MOMI);
+        SPI_PIN = (1<<SPI_SCK);
+        SPI_PIN = (1<<SPI_SCK);         // toggle SCK
+        cbi (SPI_PORT, SPI_MOMI);
+        dataout <<= 1;                     
+    }while(--bits);
+
+    cbi (SPI_DDR, SPI_MOMI);        // input mode
 }
 
