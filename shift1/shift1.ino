700f9759dfb9694b14b9d2863814a51d6f77613d
Nerd Ralph <ralphdoncaster@gmail.com>
2018-06-06 13:03:48 -0300

    initial version

---
+++ shift1/shift1.ino
@@ -0,0 +1,34 @@
+const uint8_t SHFT = 0;
+
+void shiftOne(uint8_t pin, uint8_t value) {
+  const uint8_t pinMask = 1<<pin;
+  uint8_t i = 8;
+  do
+  {
+    PORTB |= 1<<pin;
+    if (value & 0x80) PINB = pinMask;
+    PINB = pinMask;
+    asm("sbrs %0,7\nlpm":: "r"(value):"r0");
+    value <<= 1;
+    i--;
+  }
+  while(i);
+  asm("nop");
+  PORTB |= 1<<pin;   // leave pin high
+}
+
+void setup() {
+  pinMode(SHFT,OUTPUT);
+  digitalWrite(SHFT, HIGH);
+}
+
+void loop() {
+  static uint8_t level = 0;
+  for (uint8_t i = 255; i--;){
+    if (i >= level) shiftOne(SHFT, 0xFF);
+    else shiftOne(SHFT, 0x00);
+    delayMicroseconds(10);
+  }
+  level++;
+}
+
