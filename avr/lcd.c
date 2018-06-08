d1ff14e56ac3998a47a377444bed0bd524e28434
Nerd Ralph <ralphdoncaster@gmail.com>
2018-06-08 19:53:50 -0300

    baseline cleanup

--- avr/lcd.c
+++ avr/lcd.c
@@ -1,9 +1,6 @@
-// Nerd Ralph 2014 public domain software
+// Nerd Ralph 2014, 2018 public domain software
 // Control 2-line HD44780 LCD with 4 bit interface
 // based on code:
-// Copyright (C) 2012 Joonas Pihlajamaa. Released to public domain.
-// also based on code by Peter Fleury
-// http://homepage.hispeed.ch/peterfleury/avr-software.html#libs
 
 /*
 Suggested wiring between LCD module and Arduino Pro Mini
@@ -16,13 +13,13 @@ Register Select 4------------*
     Read/Write  5--Gnd       |
         Enable  6---Rx (PD0) |
         Data0   7   Rst      |
-        Data0   8   Gnd      |
-        Data0   9   2 (PD2)--*
-        Data0   10  3 (PD3)
-        Data0   11--4 (PD4)
-        Data0   12--5 (PD5)
-        Data0   13--6 (PD6)
-        Data0   14--7 (PD7)
+        Data1   8   Gnd      |
+        Data2   9   2 (PD2)--*
+        Data3   10  3 (PD3)
+        Data4   11--4 (PD4)
+        Data5   12--5 (PD5)
+        Data6   13--6 (PD6)
+        Data7   14--7 (PD7)
 */
 #define byte unsigned char
 
@@ -38,39 +35,44 @@ Register Select 4------------*
 
 #define RS_PIN (1<<PD2)
 #define EN_PIN (1<<PD0)
+#define RW_PIN (1<<PD1)
 #endif
 
 #define LCD_ISR_SAFE    0
 
 /* instruction register commands, see HD44780U data sheet */
 #define LCD_CLR               (1<<0)  /* DB0: clear display                  */
-#define LCD_HOME              (1<<1)  /* DB1: return to home position        */
 #define LCD_ENTRY_MODE        (1<<2)  /* DB2: set entry mode                 */
-#define LCD_ENTRY_INC         (1<<1)  /*   DB1: 1=increment, 0=decrement     */
-#define LCD_ENTRY_SHIFT       (1<<0)  /*   DB2: 1=display shift on           */
-#define LCD_ON                (1<<3)  /* DB3: turn lcd/cursor on             */
-#define LCD_ON_DISPLAY        (1<<2)  /*   DB2: turn display on              */
-#define LCD_ON_CURSOR         (1<<1)  /*   DB1: turn cursor on               */
-#define LCD_ON_BLINK          (1<<0)  /*     DB0: blinking cursor ?          */
-#define LCD_MOVE              (1<<4)  /* DB4: move cursor/display            */
-#define LCD_MOVE_DISP         (1<<3)  /*   DB3: move display (0-> cursor) ?  */
-#define LCD_MOVE_RIGHT        (1<<2)  /*   DB2: move right (0-> left) ?      */
-#define LCD_FUNCTION          (1<<5)  /* DB5: function set                   */
-#define LCD_FUNCTION_8BIT     (1<<4)  /*   DB4: set 8BIT mode (0->4BIT mode) */
-#define LCD_FUNCTION_2LINES   (1<<3)  /*   DB3: two lines (0->one line)      */
+#define LCD_INC               (1<<1)  /*   DB1: 1=increment, 0=decrement     */
+#define LCD_SHIFT             (1<<0)  /*   DB2: 1=display shift on           */
+/* DB3: control lcd/cursor, DB2 display on/off */
+#define LCD_ON                ((1<<3)|(1<<2))
+#define LCD_CURSOR            (1<<1)  /*   DB1: turn cursor on               */
+#define LCD_BLINK             (1<<0)  /*     DB0: blinking cursor ?          */
+#define LCD_FUNC              (1<<5)  /* DB5: function set                   */
+#define LCD_FUNC_8BIT         (1<<4)  /*   DB4: set 8BIT mode (0->4BIT mode) */
+#define LCD_FUNC_2LINES       (1<<3)  /*   DB3: two lines (0->one line)      */
 #define LCD_DDRAM             (1<<7)  /* DB7: set DD RAM address             */
 
-#define SET_CTRL_BIT(pin) ((DATA_PORT) |= pin)
-#define CLEAR_CTRL_BIT(pin) ((DATA_PORT) &= ~pin)
+/*
+const __flash char init_seq[] = 
+    { 0b00110011,               // set 8-bit mode 2x
+      0b00110010,               // set 8-bit then 4-bit mode
+      LCD_FUNC|LCD_FUNC_2LINES,
+
+*/
+
+#define sbi_lcd(pin) ((DATA_PORT) |= pin)
+#define cbi_lcd(pin) ((DATA_PORT) &= ~pin)
 
 void lcd_delay() {
-    // delay 65us for comands
-    _delay_us(65);
-}    
+    // delay 45us for comands for a margin over 37us spec
+    _delay_us(45);
+}
 
 void lcd_long_delay() {
-    // delay = 2600us;
-    _delay_us(2600);
+    // 1.85ms delay gives safe margin over 1.52ms spec
+    _delay_us(1850);
 }    
 
 // assumes EN_PIN is LOW in the beginning
@@ -78,7 +80,7 @@ void lcd_long_delay() {
 void lcd_write_nibble( const byte data ) {
     byte tmp;
     
-    SET_CTRL_BIT(EN_PIN);
+    sbi_lcd(EN_PIN);
 
 #if ( LCD_ISR_SAFE == 1 )
     cli();
@@ -89,23 +91,17 @@ void lcd_write_nibble( const byte data ) {
     sei();
 #endif
 
-    CLEAR_CTRL_BIT(EN_PIN);
+    cbi_lcd(EN_PIN);
 }
 
 void lcd_write( const byte data, const char rs) {
     if(rs == 0)
-        CLEAR_CTRL_BIT(RS_PIN); 
+        cbi_lcd(RS_PIN); 
 
     lcd_write_nibble(data);
     lcd_write_nibble(data << 4);
-#if ( LCD_ISR_SAFE == 1 )
-    cli();
-#endif
-    DATA_PORT |= (0xF0 | RS_PIN);    // high while idle
-#if ( LCD_ISR_SAFE == 1 )
-    sei();
-#endif
-    lcd_delay();          // wait for instruction to complete
+    sbi_lcd(RS_PIN);        // set for next write
+    lcd_delay();            // wait for instruction to complete
 }
 
 void lcd_command(const byte command) {
@@ -130,41 +126,39 @@ void lcd_newline(){
 }
 
 void lcd_cursor_on(){
-    lcd_command(LCD_ON|LCD_ON_DISPLAY|LCD_ON_CURSOR|LCD_ON_BLINK);
+    lcd_command(LCD_ON|LCD_CURSOR|LCD_BLINK);
 }
 
 void lcd_cursor_off(){
-    lcd_command(LCD_ON|LCD_ON_DISPLAY);
+    lcd_command(LCD_ON);
 }
 
 inline void lcd_init() {
-    // tie low for write mode
-    CLEAR_CTRL_BIT(RS_PIN);
-    CLEAR_CTRL_BIT(EN_PIN);
-
-    // DDR address is one less than port
-    DATA_PORT_DIR = 0xF0 | EN_PIN | RS_PIN;
+    // default PORT state is low
+    DATA_PORT_DIR = 0xF0 | RW_PIN | EN_PIN | RS_PIN;
 
     // if sut fuses = 4ms or 64ms(default), no need to wait before init
-    // lcd_long_delay();
-
-    lcd_write_nibble(LCD_FUNCTION|LCD_FUNCTION_8BIT);
     lcd_delay();
-
-    lcd_write_nibble(LCD_FUNCTION|LCD_FUNCTION_8BIT);
+    lcd_write_nibble(LCD_FUNC|LCD_FUNC_8BIT);
+    //lcd_long_delay();
+    lcd_delay();
+    lcd_write_nibble(LCD_FUNC|LCD_FUNC_8BIT);
+    lcd_delay();
+    lcd_write_nibble(LCD_FUNC|LCD_FUNC_8BIT);
     lcd_delay();
 
     // set 4-bit mode
-    lcd_write_nibble(LCD_FUNCTION);
+    lcd_write_nibble(LCD_FUNC);
     lcd_delay();
 
     // 2 lines, normal font, 4 bit
-    lcd_command(LCD_FUNCTION|LCD_FUNCTION_2LINES);
+    lcd_command(LCD_FUNC|LCD_FUNC_2LINES);
 
-    lcd_clear();
 
     // entry mode increment, don't shift
-    lcd_command(LCD_ENTRY_MODE|LCD_ENTRY_INC);
+    lcd_command(LCD_ENTRY_MODE|LCD_INC);
+
+    lcd_clear();
 
     // display on, no cursor
     lcd_cursor_off();
