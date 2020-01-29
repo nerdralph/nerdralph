a07fd0d4c1075db26fbe3d4cf3fd561aaaf2935a
Nerd Ralph <ralphdoncaster@gmail.com>
2020-01-29 11:32:23 -0400

    experimental development version

---
+++ picoUART/pu_devel.h
@@ -0,0 +1,160 @@
+/* optimized half-duplex high-speed AVR serial uart
+ * Ralph Doncaster 2020 open source MIT license
+ * open-drain UART requires external pullup between 4.7k and 10k
+ * if remote Rx line does not already have a pullup
+ *
+ * picoUART is accurate to the cycle (+- 0.5 cycle error)
+ * 0.64% error at 115.2k/8M and 0.4% error at 115.2k/9.6M
+ *
+ * define BAUD_RATE before including BBUart.h to change default baud rate 
+ *
+ * capable of single-pin operation (PU_TX = PU_RX)
+ * 
+ * 20200123 version 0.5
+ */
+
+#pragma once
+
+#include <avr/io.h>
+
+#ifndef BAUD_RATE
+#define BAUD_RATE 115200L               // default baud rate
+#endif
+
+#ifndef PU_TX
+#define PU_TX B,1
+#define PU_RX B,0
+#endif
+
+// use static inline functions for type safety
+extern inline float BIT_CYCLES() {return F_CPU/(BAUD_RATE*1.0);}
+
+// delay based on cycle count of asm code + 0.5 for rounding
+extern inline int PUTXWAIT() {return BIT_CYCLES() - 7 + 0.5;}
+extern inline int PURXWAIT() {return BIT_CYCLES() - 5 + 0.5;}
+
+// correct for PURXWAIT skew in PURXSTART calculation
+// skew is half of 7 delay intervals between 8 bits
+extern inline float PUSKEW() {
+    return (BIT_CYCLES() - (int)(BIT_CYCLES() + 0.5)) * 3.5;
+}
+// Time from falling edge of start bit to sample 1st bit is 1.5 *
+// bit-time. Subtract 2 cycles for sbic, 1 for lsr, and PURXWAIT.
+// Subtract 1.5 cycles because start bit detection is accurate to
+// +-1.5 cycles.  Add 0.5 cycles for int rounding, and add skew.
+extern inline int PURXSTART() {
+    return (BIT_CYCLES()*1.5 -3 -PURXWAIT() -1 +PUSKEW());
+}
+
+// I/O register macros
+#define BIT(r,b)    (b)
+#define PORT(r,b)   (PORT ## r)
+#define DDR(r,b)    (DDR ## r)
+#define PIN(r,b)    (PIN ## r)
+#define bit(io)     BIT(io)
+#define port(io)    PORT(io)
+#define ddr(io)     DDR(io)
+#define pin(io)     PIN(io)
+
+// use up registers so only r18 & r19 are free for the compiler 
+#define alloc_regs()\
+    register long long dummy1 asm("r20");\
+    asm volatile ("" : "=r" (dummy1));\
+    register int dummy2 asm("r30");\
+    asm volatile ("" : "=r" (dummy2));
+
+#define touch_regs()\
+    asm volatile ("" :: "r" (dummy1));\
+    asm volatile ("" :: "r" (dummy2));
+
+#define touch_var(v) asm volatile ("" :: "r" (v))
+
+//__attribute((used))
+__attribute((naked))
+void _pu_tx()
+{
+    alloc_regs();
+    register char c asm("r26");
+    register char bitcnt asm("r27");
+    asm volatile (
+    "ldi %[bitcnt], 10\n"               // start + 8bit + stop = 10 bits
+    "cli\n"
+    "sbi %[tx_ddr], %[tx_pin]\n"        // start bit 
+    "in r0, %[tx_ddr]\n"                // save DDR in r0
+    "com %[c]\n"                        // invert for open drain
+    "Ltxbit:\n"
+    : [c] "+r" (c),
+      [bitcnt] "+r" (bitcnt)
+    : [tx_ddr] "I" (_SFR_IO_ADDR(ddr(PU_TX))),
+      [tx_pin] "I" (bit(PU_TX))
+    );
+    __builtin_avr_delay_cycles(PUTXWAIT());
+    // 7 cycle loop
+    asm volatile (
+    "bst %[c], 0\n"                     // store lsb in T
+    "bld r0, %[tx_pin]\n"
+    "lsr %[c]\n"                        // shift for next bit
+    "dec %[bitcnt]\n"
+    "out %[tx_ddr], r0\n"
+    "brne Ltxbit\n"
+    "reti\n"                            // return & enable interrupts
+    : [c] "+r" (c),
+      [bitcnt] "+r" (bitcnt)
+    : [tx_ddr] "I" (_SFR_IO_ADDR(ddr(PU_TX))),
+      [tx_pin] "I" (bit(PU_TX))
+    );
+    touch_regs();
+}
+
+inline void pu_tx(char c)
+{
+    asm volatile ("" : "+x"(c) :: "r18", "r19", "r27");
+    _pu_tx();
+}
+
+
+//__attribute((used))
+__attribute((naked))
+char _pu_rx()
+{
+    alloc_regs();
+    register char c asm("r26");
+    asm volatile (
+    // wait for idle state (high)
+    "1: sbis %[rx_pin], %[rx_bit]\n"
+    "rjmp 1b\n"
+    "ldi %[c], 0x80\n"                  // bit shift counter
+    "cli\n"
+    // wait for start bit (low)
+    "1: sbic %[rx_pin], %[rx_bit]\n"
+    "rjmp 1b\n"
+    : [c] "=d" (c)
+    : [rx_pin] "I" (_SFR_IO_ADDR(pin(PU_RX))),
+      [rx_bit] "I" (bit(PU_RX))
+    );
+    __builtin_avr_delay_cycles(PURXSTART());
+    asm volatile ("Lrxbit:");
+    __builtin_avr_delay_cycles(PURXWAIT());
+    // 5 cycle loop 
+    asm volatile (
+    "lsr %[c]\n" 
+    "sbic %[rx_pin], %[rx_bit]\n"
+    "ori %[c], 0x80\n"
+    "brcc Lrxbit\n"
+    "reti\n"
+    : [c] "+d" (c)
+    : [rx_pin] "I" (_SFR_IO_ADDR(pin(PU_RX))),
+      [rx_bit] "I" (bit(PU_RX))
+    );
+    touch_regs();
+}
+
+inline char pu_rx()
+{
+    register char c asm("r26");
+    asm volatile ("" : "=r"(c) :: "r18", "r19", "memory");
+    _pu_rx();
+    asm volatile ("" :: "r" (c) : "memory");
+    return c;
+}
+
