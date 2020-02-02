6d75e50f20c3c6f3d60893acafeb8e12047c8491
Nerd Ralph <ralphdoncaster@gmail.com>
2020-02-02 16:52:20 -0400

    add prints_P and echo test/example

---
+++ picoUART/examples/echo/echo.ino
@@ -0,0 +1,9 @@
+// picoUART echo test
+
+#include <picoUART.h>
+#include <pu_print.h>
+
+void setup() {}
+
+// see pu_print.c for test loop() code
+
---
+++ picoUART/pu_print.h
@@ -0,0 +1,12 @@
+// print using picoUART
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// output null-terminated string in flash
+void prints_P(const __flash char*);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
