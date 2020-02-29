89318581e09a4c2e89e38038dbcf0a746f10e5be
Nerd Ralph <ralphdoncaster@gmail.com>
2020-02-29 12:01:52 -0400

    simplify example and add total code use

--- avr/touch.c
+++ avr/touch.c
@@ -1,53 +1,53 @@
 /* tiny touch sense example for AVR MCUs
  * touch sense code takes only 40 bytes of flash
+ * <100 bytes flash  use on a t13 with interrupt vectors and main overhead
  * Ralph Doncaster 2020 open source MIT license
  */
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-const unsigned TOUCH_GPIO = 3;
-const unsigned LED_GPIO = 4;
+const uint8_t TOUCH_GPIO = 3;
+const uint8_t LED_GPIO = 4;
 
-uint8_t rise_time()
+// valid rise time range = 1-255
+// zero indicates overflow; likely excessive capacitance or a short
+// GPIO = 8-bit port mask i.e. (1 << PB4)
+uint8_t rise_time(uint8_t gpio)
 {
     uint8_t time = 0;
-    DDRB &= ~(1<<TOUCH_GPIO);           // input mode
+    // we need more samples at lower clock speeds
+    uint8_t samples = (uint8_t)(24e6/F_CPU + 0.5);
     do {
-        PINB = (1<<TOUCH_GPIO);           // pullup on
-        PINB = (1<<TOUCH_GPIO);           // pullup off
-        time++;
-    } while (bit_is_clear(PINB, TOUCH_GPIO));
-    DDRB |= (1<<TOUCH_GPIO);            // discharge
+        DDRB |= gpio;               // discharge pad
+        DDRB &= ~(gpio);            // input mode
+        do {
+            PINB = gpio;            // pullup on
+            PINB = gpio;            // pullup off
+            time++;
+        } while ( time && !(PINB & gpio) );
+    } while (--samples);
     return time;
 }
 
-
-_Bool tsense(uint8_t threshold)
+int main()
 {
-    return ( rise_time() > (uint8_t)(threshold+1)  );
-}
-
-
-__attribute((noinline))
-void wait_ms(uint16_t count)
-{
-    while (--count) _delay_us(999.5);
-}
-
-void main()
-{
-    uint8_t no_touch;
     DDRB |= (1<<LED_GPIO);              // output mode
-    // calibrate sensitivity
-    rise_time();                        // discard 1st sample
-    no_touch = rise_time();
-forever:
-    if ( tsense(no_touch) ) {
-        PINB |= (1<<LED_GPIO);          // toggle LED
-        wait_ms(1000);
+    uint8_t threshold = 22;
+    while (1) {
+        uint8_t delay = 0;
+        if ( rise_time(1 << TOUCH_GPIO) > threshold ) {
+            PINB |= (1<<LED_GPIO);      // toggle LED
+            _delay_ms(1000);            // wait for touch release
+        }
+        /*
+        uint8_t t2 = rtime(1 << PB2);
+        if ( t2 > 28 ) {
+            // eelog(t2);
+            PINB |= (1<<LED_GPIO);      // toggle LED
+            wait_ms(1000);              // wait for touch release
+        }
+        */
     }
-    wait_ms(100);
-    goto forever;
 }
 
